e7e32fc581f7369eba41d4325ed41f3476d2d16a
Arina <118733508+arinachernova1@users.noreply.github.com>
2023-05-15 22:02:36 +0300

    Add files via upload

---
+++ Phones/Phones/backup/edit.pas
@@ -0,0 +1,48 @@
+unit Edit;
+
+{$mode ObjFPC}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, Buttons,
+  ExtCtrls;
+
+type
+
+  { TfEdit }
+
+  TfEdit = class(TForm)
+    bSave: TBitBtn;
+    BitBtn2: TBitBtn;
+    CBNote: TComboBox;
+    eTelephone: TEdit;
+    eName: TEdit;
+    Image1: TImage;
+    Image2: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    procedure FormShow(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  fEdit: TfEdit;
+
+implementation
+
+{$R *.lfm}
+
+{ TfEdit }
+
+procedure TfEdit.FormShow(Sender: TObject);
+begin
+  eName.SetFocus;
+end;
+
+end.
+
